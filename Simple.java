38d57a9c6529e2008786fc5b35dcf9b783f531a0
Sunidhi Singh <75873828+Sunport15@users.noreply.github.com>
2024-09-06 10:46:13 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,6 +1,10 @@
-public class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
+public class Simple {  
+    public static void main(String args[]) {  
+        printMessage("Hello Java");
+        printMessage("Welcome to Java Programming!");
     }  
 
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
 }
